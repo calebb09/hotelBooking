c9a953ba76cb3a53371eb66eda09e7efe0c18c43
calebb09 <67234586+calebb09@users.noreply.github.com>
2025-12-14 15:31:01 +0300

    update payment.js
    hotelBooking declaration created

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -245,6 +245,7 @@ exports.directPay = async (req, res, next) => {
     let body = req.body;
     let create_query = {};
     let errors = [];
+    let hotelBooking = [];
     let entry = {};
     let price_query = {};
     let userId = null;
