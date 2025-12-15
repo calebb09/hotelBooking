fc9630a42f926b34f495a80292e4023dac20f81e
Caleb <caleb090@outlook.com>
2025-12-15 14:50:10 +0300

    update db.js
    db retry 10 second

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,15 @@ module.exports = {
   apps: [
     {
       name: "backend",
-      script: "./app.js",        // or app.js / index.js
-      instances: 1,                 // or "max" for cluster mode
+      script: "./app.js", // or app.js / index.js
+      instances: 1, // or "max" for cluster mode
       exec_mode: "fork",
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
-}
-
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
